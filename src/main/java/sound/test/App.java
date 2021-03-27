4fff06c937d5d647730b52921442bde787d1bf47
STMA Robotics <7028Robotics 7028Robotics@mystma.org>
2021-03-27 11:02:11 -0500

    VolumeTuning #7

--- src/main/java/sound/test/App.java
+++ src/main/java/sound/test/App.java
@@ -114,9 +114,9 @@ public class App {
     promotionClip = AudioSystem.getClip();
     promotionClip.open(AudioSystem.getAudioInputStream(ClassLoader.getSystemResourceAsStream("promotion.wav")));
 
-    setVolume(idleClip,0.1);
-    setVolume(slowClip,0.2);
-    setVolume(fastClip,0.3);
+    setVolume(idleClip,0.05);
+    setVolume(slowClip,0.1);
+    setVolume(fastClip,0.2);
 
     while(!inst.isConnected()) {
       Thread.sleep(1000);
